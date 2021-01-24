9f3c6f1bf163e5b1f2d9d7822e5e77c20ee55c55
Al-Azif <alazif@miskatonic.io>
2021-01-23 16:14:38 -0800

    Add debug socket

--- source/main.c
+++ source/main.c
@@ -1,3 +1,7 @@
+//#define DEBUG_SOCKET
+#define DEBUG_IP "192.168.2.2"
+#define DEBUG_PORT 9023
+
 #include "ps4.h"
 
 int _main(struct thread *td) {
@@ -5,6 +9,11 @@ int _main(struct thread *td) {
 
   initKernel();
   initLibc();
+
+#ifdef DEBUG_SOCKET
+  initNetwork();
+  DEBUG_SOCK = SckConnect(DEBUG_IP, DEBUG_PORT);
+#endif
 
   jailbreak();
 
@@ -19,6 +28,11 @@ int _main(struct thread *td) {
   mkdir("/update/PS4UPDATE.PUP", 777);
 
   printf_notification("Disabled updates!");
+
+#ifdef DEBUG_SOCKET
+  printf_socket("\nClosing socket...\n\n");
+  SckClose(DEBUG_SOCK);
+#endif
 
   return 0;
 }
